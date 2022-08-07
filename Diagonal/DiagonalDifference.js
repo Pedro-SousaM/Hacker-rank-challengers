826d816b0df109de4a9673a5cd2e9581e3b65ee8
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2022-08-06 21:09:35 -0300

    Create DiagonalDifference.js

---
+++ Diagonal/DiagonalDifference.js
@@ -0,0 +1,19 @@
+function diagonalDifference(arr) {
+    const l = arr.length-1
+    let X1numbers = null 
+    let X2numbers = null
+    for(var x=0;x<=l; x++ ){
+         X1numbers += arr[x][x] 
+         X2numbers += arr[x][l-x] 
+    } 
+    console.log(X1numbers, X2numbers)
+    const check = [X1numbers, X2numbers] 
+    function compare(a,b){
+        if(a>b)return 1; 
+        if(a<b)return -1; 
+        return 0
+    }
+    const progressive = check.sort(compare) 
+    return progressive[1]-progressive[0]
+
+}
